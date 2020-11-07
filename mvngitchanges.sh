532c5d069c541d6475f0e7d3b5a5366d0514e701
Jul <julschsl@mailbox.org>
2020-11-07 17:08:14 +0100

    Use "git diff" instead of "git status" to detect changed modules

--- mvngitchanges.sh
+++ mvngitchanges.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 mvn_git_changes(){
+
+    branch="$1"
+    [ -z "$branch" ] && echo "Usage: mvn_git_changes <branch or commit> <one or more goals like install/build/clean>" && return
+    shift
+
     goals="$*"
     [ -z "$goals" ] && echo "Usage: mvn_git_changes [one or more goals like install/build/clean]" && return
     
-    changed_modules=($(git status | grep --color=never -E "modified:|deleted:|added:" | grep --color=never -oP " \K\S*(?=(\/pom\.xml|\/src\/))" | uniq))
+    changed_modules=($(git diff --name-only $branch | grep --color=never -oP "\K\S*(?=(\/pom\.xml|\/src\/))" | uniq))
     existing_changed_modules=()
     for module in "${changed_modules[@]}"
     do
@@ -15,9 +20,9 @@ mvn_git_changes(){
     unset changed_modules
 
     if [ ${#existing_changed_modules[@]} -eq 0 ]; then
-        echo "No changed (modified/deleted/added) modules found."
+        echo "No changed (modified/deleted/added) files found in maven modules."
     else
-        echo "Found changed modules: `printf "%s " "${existing_changed_modules[@]}"`"
+        echo "Found changed files in modules: `printf "%s " "${existing_changed_modules[@]}"`"
         echo "Executing command: mvn $goals -amd -pl `printf "%s," "${existing_changed_modules[@]}"`"
         mvn $goals -amd -pl $(printf "%s," "${existing_changed_modules[@]}")
     fi
